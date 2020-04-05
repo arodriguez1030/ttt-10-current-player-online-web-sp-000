b00faa7096ae2cb281864104895427b50c7e3726
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-05 20:56:39 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+
+def turn_count(board)
+  counter= 0
+  board.each do |move|
+    if move  == "X" || move == "O"
+    counter += 1
+  # returns the number of turns that have been played
+else 
+end
+end 
+end
